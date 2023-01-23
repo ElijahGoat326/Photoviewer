0e30461d4f8cefd19cfb4e46049799662ee36346
ElijahGoat326 <elijahlopez326@gmail.com>
2023-01-23 15:00:33 -0500

    Added 2 buttons

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 import java.io.FileInputStream;
 
 public class Main extends Application {
-
+    int currentImage;
         @Override
         public void start(Stage primaryStage) throws Exception {
             // Construct controls
@@ -36,14 +36,59 @@ public class Main extends Application {
             Image image5 = new Image(input5);
             imageView.setImage(image5);
 
-            int currentImage = 5;
+            currentImage = 5;
 
             Button button = new Button("My Label");
-            button.setText("Click me if you dare!");
+            button.setText("Forward!");
+            button.setOnAction(actionEvent -> {
+                System.out.println(currentImage);
+                if (currentImage == 5) {
+                    imageView.setImage(image);
+                    currentImage = 1;
+                } else if (currentImage == 1) {
+                    imageView.setImage(image2);
+                    currentImage = 2;
+                } else if (currentImage == 2) {
+                    imageView.setImage(image3);
+                    currentImage = 3;
+                } else if (currentImage == 3) {
+                    imageView.setImage(image4);
+                    currentImage = 4;
+                } else if (currentImage == 4) {
+                    imageView.setImage(image5);
+                    currentImage = 5;
+                }
+            });
+
+
+            Button button2 = new Button("My Label");
+            button2.setText("Reverse!");
+            button2.setOnAction(actionEvent -> {
+                System.out.println(currentImage);
+                if (currentImage == 1) {
+                    imageView.setImage(image5);
+                    currentImage = 5;
+                } else if (currentImage == 5) {
+                    imageView.setImage(image4);
+                    currentImage = 4;
+                } else if (currentImage == 4) {
+                    imageView.setImage(image3);
+                    currentImage = 3;
+                } else if (currentImage == 3) {
+                    imageView.setImage(image2);
+                    currentImage = 2;
+                } else if (currentImage == 2) {
+                    imageView.setImage(image);
+                    currentImage = 1;
+                }
+            });
+
+
+
 
             // Layout the controls
-            HBox hbox = new HBox(imageView);
-            Scene scene = new Scene(hbox, 400, 200);
+            HBox hbox = new HBox(imageView,button,button2);
+            Scene scene = new Scene(hbox, 600, 400);
 
             // Put the layout into the scene
             primaryStage.setTitle("ImageView Experiment 1");
